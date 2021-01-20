00d26440d6f4356e27d8ddea00302ecf488e2d97
Zachary Munson <73870082+zmunson85@users.noreply.github.com>
2021-01-20 12:54:37 -0700

    *Notes*
    This is for learning purposes, it will help me reference each operation or method later and I like to leave notes that will help me understand the concepts. Also during assignments like this I reached out for help many times and needed to document what changes were made with the help of TA's or other Cohort mates.

--- part1/server/config/mongoose.config.js
+++ part1/server/config/mongoose.config.js
@@ -1,7 +1,10 @@
+//LINE 1---------IF NOT COMPLETE ALREADY***INSTALL MODULES*** then>>> Import Mongoose from NodeMods Folder.
+//LINE 5_________________**CONNECT config.js to "products" DB using mongoose.connect**//this sets our db to be {state} essentially.
+
 const mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost/products", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-    .then(() => console.log("You are now in the mainframe!!"))
-    .catch(() => console.log("MELT DOWN MELT DOWN"))
+    .then(() => console.log("You are now in the mainframe!!"))     //this will show proof of server life, in the server terminal its like a success msg
+    .catch(() => console.log("MELT DOWN MELT DOWN"))                 // catch works like a validation, it is telling you there are mistakes in the server, (adverse success/else)
